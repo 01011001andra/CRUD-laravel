53e018b5f0a61812ba1d92ed8962b072de2858d5
Yandra Muslim <yandrayt1@gmail.com>
2022-12-08 12:43:07 +0700

    ss

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,4 @@ use App\Http\Controllers\dataBarangController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::resource('/', databarangController::class);
 Route::resource('/data', databarangController::class);
