7f19dcae7e8f5434e9eebff8e1b5f9b2dd0c825f
Yandra Muslim <yandrayt1@gmail.com>
2022-12-08 12:55:53 +0700

    hello

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use App\Http\Controllers\dataBarangController;
 use Illuminate\Support\Facades\Route;
 
-
+Route::get('/', function () {
+  return view('crud/welcome');
+});
 Route::resource('/data', databarangController::class);
