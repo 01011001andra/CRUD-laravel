021d507ba46b47b06b789f7f93d8dd3210f3d673
Yandra Muslim <yandrayt1@gmail.com>
2022-12-12 13:41:26 +0700

    a

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
   return view('crud/welcome');
 });
-Route::resource('/data', databarangController::class);
+
+
+Route::get('/data', [databarangController::class, "index"]);
+Route::get('/data/create', [databarangController::class, "create"]);
