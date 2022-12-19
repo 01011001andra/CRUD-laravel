36619b816494c91eb8b643e79c7ed0cfc21cfb47
Yandra Muslim <yandrayt1@gmail.com>
2022-12-19 17:04:17 +0700

    sdn

--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,5 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
   return view('crud/dashboard');
 });
+// Route::resource('/data', databarangController::class);
 Route::get('/data', [databarangController::class, "index"]);
